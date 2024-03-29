4edb4a7a81068af009ce297c7f25581687a252bd
Rafsanjlil <tampabanan@gmail.com>
2024-03-30 03:06:23 +0700

    fix main activity and gradle

--- app/src/main/java/com/asim/synergy/MainActivity.kt
+++ app/src/main/java/com/asim/synergy/MainActivity.kt
@@ -2,10 +2,25 @@ package com.asim.synergy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asim.synergy.databinding.ActivityMainBinding
+import com.asim.synergy.databinding.CardTotalCostBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val totalLayanan = 1000
+    val totalTip = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Akses TextView di dalam layout card
+        val cardBinding = CardTotalCostBinding.bind(findViewById(R.id.card_total_cost))
+        cardBinding.textSubtotal.text =
+            getString(R.string.currency) + totalLayanan.toString() +" + "+ getString(R.string.currency) + totalTip.toString()
+        cardBinding.textMargintotal.text=(totalLayanan+totalTip).toString()
+
     }
 }
